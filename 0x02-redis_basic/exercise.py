e3dd8d2efaabc4b86a1843c7916df4537071011a
kassem00 <kamsdonga@gmail.com>
2024-08-14 19:40:12 +0000

    au

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """ Cache class """
 
-
 import redis
 import uuid
-from typing import Union
-
+from typing import Union, Callable
 
 class Cache:
     """ Cache class """
@@ -19,3 +17,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None):
+        """Retrieve data from Redis and apply an optional conversion function."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Helper method to retrieve a string from Redis"""
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Helper method to retrieve an integer from Redis"""
+        return self.get(key, int)
